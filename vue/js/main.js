2682cf3a3af407cb2aaf564e0c15465c037d6187
nastyaimpress666 <nastya.nastya07012@gmail.com>
2026-03-03 19:20:20 +0700

    done task 5

--- vue/js/main.js
+++ vue/js/main.js
@@ -7,8 +7,18 @@ let app = new Vue({
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         instock: true,
         onSale: true, 
-        details: ['80% cotton', '20% polyester', 'Gender-neutral']
-
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
     }
 
 })
