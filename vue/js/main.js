002d85953b493279cef0e499dfb734caca322077
nastyaimpress666 <nastya.nastya07012@gmail.com>
2026-03-03 18:25:24 +0700

    first commit

---
+++ vue/js/main.js
@@ -0,0 +1,18 @@
+let app = new Vue({
+   el: '#app',
+   data: {
+        product: "Socks",
+        image: "./assets/vmSocks-green-onWhite.jpg",
+        altText: "A pair of socks",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        instock: true,
+        onSale: true, 
+        details: ['80% cotton', '20% polyester', 'Gender-neutral']
+
+    }
+
+})
+
+   
+
+
